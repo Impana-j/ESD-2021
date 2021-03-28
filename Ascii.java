500e0c8ffc86a30370eacaaf6c1c0daec5011fbd
impana-j! <impana.k@learner.manipal.edu>
2021-03-28 21:47:21 +0530

    Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,21 @@
+ class Ascii {
+	public static void main(String[] args) 
+	{
+		int i;
+		
+		for(i = 65; i <= 90; i++)
+		{
+			System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+		}
+		
+		for(i = 97; i <= 122; i++)
+		{
+			System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+		}
+		
+		for(i = 48; i <= 57; i++)
+		{
+			System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+		}
+	}
+}
