166ec3dc355fb7f8b3f86b7e584f8bb95fa222df
impana-j! <impana.k@learner.manipal.edu>
2021-03-28 21:10:46 +0530

    Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+class Armstrong {
+
+    public static void main(String[] args) {
+
+       int num = 243;
+        int temp , remainder, result = 0;
+
+        	temp = num;
+
+        while (temp != 0)
+        {
+            remainder = temp % 10;
+            result += Math.pow(remainder, 3);
+            temp /= 10;
+        }
+
+        if(result == num)
+            System.out.println(num + " is an Armstrong number.");
+        else
+            System.out.println(num + " is not an Armstrong number.");
+    }
+}
