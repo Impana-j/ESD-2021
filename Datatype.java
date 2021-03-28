0f935ca2bffacd54c3881a24f9ddef9b8912178b
impana-j! <impana.k@learner.manipal.edu>
2021-03-28 21:40:34 +0530

    Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+class Datatype { 
+	public static void main(String args[]) 
+	{ 
+		char a = 'I'; 
+		int i = 39; 
+		byte b = 8;  
+		short s = 42; 
+		double d = 6.155453532; 
+		float f = 2.7333434f; 
+
+		System.out.println("char: " + a); 
+		System.out.println("integer: " + i); 
+		System.out.println("byte: " + b); 
+		System.out.println("short: " + s); 
+		System.out.println("float: " + f); 
+		System.out.println("double: " + d); 
+	} 
+} 
