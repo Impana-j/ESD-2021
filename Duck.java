545d78c1606b275e1b6befb1f848abdd014713d3
Impana-j <impana.k@learner.manipal.edu>
2021-03-30 09:08:14 +0530

    Duck

--- Duck.java
+++ Duck.java
@@ -8,18 +8,18 @@ public class Duck {
   Scanner sc=new Scanner(System.in);
   System.out.println("Enter a number");
   int num=sc.nextInt();
-  int c = 0;
+  int i = 0;
   int r;
   while(num!=0)
   {
    r=num%10;
    if(r==0)
-    c++;
+    i++;
    num=num/10;
    
    
   }
-  if(c>0)
+  if(i>0)
    System.out.println("It is a duck number");
   else
    System.out.println("It is not a duck number");
