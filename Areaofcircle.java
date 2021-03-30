52a2059be57f0d70fe21947d185e83cef17d7a14
Impana-j <impana.k@learner.manipal.edu>
2021-03-30 19:01:48 +0530

    Areaofcircle

---
+++ Areaofcircle.java
@@ -0,0 +1,15 @@
+//46. Implement a java program to find the area of a) Circle
+import java.util.Scanner;
+public class Areaofcircle
+{
+    public static void main(String[] args) 
+    {
+        int r;
+        double pi = 3.14, area;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter radius of circle:");
+        r = s.nextInt();
+        area = pi * r * r;
+        System.out.println("Area of circle:"+area);
+    }            
+}
