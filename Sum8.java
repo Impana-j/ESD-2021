28149b0dad9f9b9d9bb8f93f9f1b66bed9c06da2
Impana-j <impana.k@learner.manipal.edu>
2021-03-30 19:38:12 +0530

    Sum8

---
+++ Sum8.java
@@ -0,0 +1,20 @@
+//9. Implement a JAVA program to find the number between 0 - 1000 where the sum is 8.
+class Sum8{
+	static int i,n,a,sum=0;
+	public static void main(String[] args) 
+	{
+		for(i=0;i<1000;i++)
+		{
+			n=i;
+			sum = 0;
+			while(n>0)
+			{
+			a = n%10;
+			n = n/10;
+			sum = sum +a;
+		    }
+			if(sum==8)
+				System.out.println(" "+i);
+	    }
+    }
+}
