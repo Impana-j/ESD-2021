d0dcd9a45dce81883cd68cb6e20025bd1423e2d1
Impana-j <impana.k@learner.manipal.edu>
2021-03-30 17:42:33 +0530

    Areaoftriangle

---
+++ Areaoftriangle.java
@@ -0,0 +1,17 @@
+//46. Implement a java program to find the area of  f) triangle
+import java.util.Scanner;
+class Areaoftriangle{
+   public static void main(String args[]) {   
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Enter the width of the Triangle:");
+      double base = scanner.nextDouble();
+
+      System.out.println("Enter the height of the Triangle:");
+      double height = scanner.nextDouble();
+
+      //Area = (width*height)/2
+      double area = (base* height)/2;
+      System.out.println("Area of Triangle is: " + area);      
+   }
+}
