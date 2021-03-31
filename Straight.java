c24690ae70cb6411abfa63ef0d91287dbc54c430
Impana-j <impana.k@learner.manipal.edu>
2021-03-31 11:36:59 +0530

    Straight

---
+++ Straight.java
@@ -0,0 +1,7 @@
+//49. Implement a java program to find the straight line equation giving input m and c.
+public class Straight{
+	public static void main(String args[]){
+		System.out.println("Straight line equation");
+		System.out.println("y = "+args[0]+"x + "+args[1]);
+	}
+}
